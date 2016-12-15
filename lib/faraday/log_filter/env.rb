adbatista/faraday-filterable-logger

first version of filterable logger

lib/faraday/filterable_logger/env.rb
@@ -1,0 +1,46 @@
+require 'forwardable'
+
+module Faraday
+  module FilterableLogger
+    class Env < SimpleDelegator
+      def initialize(env, params_filter=[])
+        super env
+        @filter = Filter.new(params_filter)
+      end
+
+      def headers
+        Array(response_headers).map { |k, v| "#{k}: #{v.inspect}" }.join("\n")
+      end
+
+      def http_verb
+        __getobj__.method
+      end
+
+      def dump_url
+        @filter.filter_url(url).to_s
+      end
+
+      def dump_body
+        if self[:body].respond_to?(:to_str)
+          self[:body].to_str
+        else
+          pretty_inspect(self[:body])
+        end
+      end
+
+      def status
+        super.to_s
+      end
+
+      def has_body?
+        !!self[:body]
+      end
+
+      private
+      def pretty_inspect(body)
+        require 'pp' unless body.respond_to?(:pretty_inspect)
+        body.pretty_inspect
+      end
+    end
+  end
+end

lib/faraday/filterable_logger/version.rb
@@ -1,0 +1,5 @@
+module Faraday
+  module FilterableLogger
+    VERSION = "0.1.0".freeze
+  end
+end

spec/faraday/filterable_logger/env_spec.rb
@@ -1,0 +1,83 @@
+require "spec_helper"
+
+describe Faraday::FilterableLogger::Env do
+  subject { described_class.new(env) }
+  let(:env) { double('Env') }
+
+  describe "#has_body?" do
+    context "when env has body" do
+      let(:env) { {body: "content" } }
+
+      it { is_expected.to have_body }
+    end
+
+    context "when env has no body" do
+      let(:env) { {} }
+
+      it { is_expected.not_to have_body }
+    end
+  end
+
+  describe "#dump_url" do
+    let(:url) { URI("http://google.com?param=value") }
+    let(:env) { double(url: url) }
+
+    it "returns the env url as string" do
+      expect(subject.dump_url).to eq("http://google.com?param=value")
+    end
+
+    it "filters the url params" do
+      expect_any_instance_of(Faraday::FilterableLogger::Filter).to receive(:filter_url).with(url)
+
+      subject.dump_url
+    end
+  end
+
+  describe "#dump_body" do
+    context "when body content is a string" do
+      let(:env) { {body: "some content"} }
+
+      it "returns the body content" do
+        expect(subject.dump_body).to eq "some content"
+      end
+    end
+
+    context "when body content is not a string" do
+      let(:body) { {foo: "bar", baz: "zaz"} }
+      let(:env) { {body: body} }
+
+      it "returns the body content" do
+        require 'pp'
+        expect(subject.dump_body).to eq body.pretty_inspect
+      end
+    end
+  end
+
+  describe "#headers" do
+    let(:headers) do
+      {
+        AUTHENTICATON: 'Bearer foo',
+        Accept: 'application/json'
+      }
+    end
+    let(:env) { double('Env', response_headers: headers) }
+
+    it "returns all headers as string" do
+      expect(subject.headers).to eq "AUTHENTICATON: \"Bearer foo\"\nAccept: \"application/json\""
+    end
+  end
+
+  describe "#status" do
+    let(:env) { double('Env', status: 200) }
+    it "return status as string" do
+      expect(subject.status).to eq "200"
+    end
+  end
+
+  describe "#http_verb" do
+    let(:env) { double("Env", method: "POST") }
+    it "returns env method" do
+      expect(subject.http_verb).to eq("POST")
+    end
+  end
+end

spec/faraday/filterable_logger/middleware_spec.rb
@@ -1,0 +1,81 @@
+require "spec_helper"
+
+require "logger"
+require "stringio"
+
+describe Faraday::FilterableLogger::Middleware do
+  let(:log) { StringIO.new }
+  let(:logger) { Logger.new(log) }
+
+  it "logs with the filtered params" do
+    connection(logger, filter: [:api]).get("/temaki?api=122435")
+    log.rewind
+    expect(log.read).to include("api=[FILTERED]")
+  end
+
+  it "logs the request method at an INFO level" do
+    connection(logger).get("/temaki?api=123456")
+    log.rewind
+    expect(log.read).to match(/\bINFO\b.+\bGET\b/)
+  end
+
+  it "logs the request URI at an INFO level" do
+    connection(logger).get("/temaki?api=123456")
+    log.rewind
+    expect(log.read).to match(%r{\bINFO\b.+\bhttp://sushi.com/temaki\b})
+  end
+
+  it "logs a response status code at an INFO level" do
+    connection(logger).get("/temaki")
+    log.rewind
+    expect(log.read).to match(/INFO.+Status: 200\b/)
+  end
+
+  it "doesn't logs request's body" do
+    connection(logger).post("/nigirizushi")
+    log.rewind
+    expect(log.readlines.join).not_to include('DEBUG -- response: {"id":"1"}')
+  end
+
+  it "logs the request's headers at DEBUG level" do
+    connection(logger).post('/nigirizushi')
+    log.rewind
+
+    expect(log.readlines.join).to include('DEBUG -- response: Content-Type: "application/json"')
+  end
+
+  context "when set body log to true" do
+    it "logs the request's body at DEBUG level" do
+      connection(logger, log_options: { bodies: true }).post('/nigirizushi')
+      log.rewind
+
+      expect(log.readlines.join).to include('DEBUG -- response: {"id":"1"}')
+    end
+  end
+
+  context "when set headers log to false" do
+    it "doesn't log the request's headers" do
+      connection(logger, log_options: { headers: false }).post('/nigirizushi')
+      log.rewind
+
+      expect(log.readlines.join).not_to include('DEBUG -- response: Content-Type: "application/json"')
+    end
+  end
+
+  private
+
+  def connection(logger = nil, config={})
+    Faraday.new(url: "http://sushi.com") do |builder|
+      builder.request(:url_encoded)
+      builder.response(:filterable_logger, logger, config)
+      builder.adapter(:test) do |stub|
+        stub.get("/temaki") do
+          [200, { "Content-Type" => "text/plain" }, "temaki"]
+        end
+        stub.post("/nigirizushi") do
+          [200, { "Content-Type" => "application/json" }, "{\"id\":\"1\"}"]
+        end
+      end
+    end
+  end
+end

spec/spec_helper.rb
@@ -1,0 +1,8 @@
+lib = File.expand_path("../lib", __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require "faraday/filterable_logger"
+
+RSpec.configure do |config|
+  config.order = :random
+end

westfieldlabs/faraday-log-filter

lib/faraday/log_filter/env.rb
@@ -1,0 +1,46 @@
+require 'delegate'
+
+module Faraday
+  module LogFilter
+    class Env < SimpleDelegator
+      def initialize(env, params_filter=[])
+        super env
+        @filter = Filter.new(params_filter)
+      end
+
+      def headers
+        Array(response_headers).map { |k, v| "#{k}: #{v.inspect}" }.join("\n")
+      end
+
+      def http_verb
+        __getobj__.method
+      end
+
+      def dump_url
+        @filter.filter_url(url).to_s
+      end
+
+      def dump_body
+        if self[:body].respond_to?(:to_str)
+          self[:body].to_str
+        else
+          pretty_inspect(self[:body])
+        end
+      end
+
+      def status
+        super.to_s
+      end
+
+      def has_body?
+        !!self[:body]
+      end
+
+      private
+      def pretty_inspect(body)
+        require 'pp' unless body.respond_to?(:pretty_inspect)
+        body.pretty_inspect
+      end
+    end
+  end
+end

lib/faraday/log_filter/version.rb
@@ -1,0 +1,5 @@
+module Faraday
+  module LogFilter
+    VERSION = "0.1.0".freeze
+  end
+end

spec/faraday/log_filter/env_spec.rb
@@ -1,0 +1,83 @@
+require "spec_helper"
+require 'uri'
+describe Faraday::LogFilter::Env do
+  subject { described_class.new(env) }
+  let(:env) { double('Env') }
+
+  describe "#has_body?" do
+    context "when env has body" do
+      let(:env) { {body: "content" } }
+
+      it { is_expected.to have_body }
+    end
+
+    context "when env has no body" do
+      let(:env) { {} }
+
+      it { is_expected.not_to have_body }
+    end
+  end
+
+  describe "#dump_url" do
+    let(:url) { URI("http://google.com?param=value") }
+    let(:env) { double(url: url) }
+
+    it "returns the env url as string" do
+      expect(subject.dump_url).to eq("http://google.com?param=value")
+    end
+
+    it "filters the url params" do
+      expect_any_instance_of(Faraday::LogFilter::Filter).to receive(:filter_url).with(url)
+
+      subject.dump_url
+    end
+  end
+
+  describe "#dump_body" do
+    context "when body content is a string" do
+      let(:env) { {body: "some content"} }
+
+      it "returns the body content" do
+        expect(subject.dump_body).to eq "some content"
+      end
+    end
+
+    context "when body content is not a string" do
+      let(:body) { {foo: "bar", baz: "zaz"} }
+      let(:env) { {body: body} }
+
+      it "returns the body content" do
+        require 'pp'
+        expect(subject.dump_body).to eq body.pretty_inspect
+      end
+    end
+  end
+
+  describe "#headers" do
+    let(:headers) do
+      {
+        AUTHENTICATON: 'Bearer foo',
+        Accept: 'application/json'
+      }
+    end
+    let(:env) { double('Env', response_headers: headers) }
+
+    it "returns all headers as string" do
+      expect(subject.headers).to eq "AUTHENTICATON: \"Bearer foo\"\nAccept: \"application/json\""
+    end
+  end
+
+  describe "#status" do
+    let(:env) { double('Env', status: 200) }
+    it "return status as string" do
+      expect(subject.status).to eq "200"
+    end
+  end
+
+  describe "#http_verb" do
+    let(:env) { double("Env", method: "POST") }
+    it "returns env method" do
+      expect(subject.http_verb).to eq("POST")
+    end
+  end
+end

spec/faraday/log_filter/middleware_spec.rb
@@ -1,0 +1,81 @@
+require "spec_helper"
+
+require "logger"
+require "stringio"
+
+describe Faraday::LogFilter::Middleware do
+  let(:log) { StringIO.new }
+  let(:logger) { Logger.new(log) }
+
+  it "logs with the filtered params" do
+    connection(logger, filter: [:api]).get("/temaki?api=122435")
+    log.rewind
+    expect(log.read).to include("api=[FILTERED]")
+  end
+
+  it "logs the request method at an INFO level" do
+    connection(logger).get("/temaki?api=123456")
+    log.rewind
+    expect(log.read).to match(/\bINFO\b.+\bGET\b/)
+  end
+
+  it "logs the request URI at an INFO level" do
+    connection(logger).get("/temaki?api=123456")
+    log.rewind
+    expect(log.read).to match(%r{\bINFO\b.+\bhttp://sushi.com/temaki\b})
+  end
+
+  it "logs a response status code at an INFO level" do
+    connection(logger).get("/temaki")
+    log.rewind
+    expect(log.read).to match(/INFO.+Status: 200\b/)
+  end
+
+  it "doesn't logs request's body" do
+    connection(logger).post("/nigirizushi")
+    log.rewind
+    expect(log.readlines.join).not_to include('DEBUG -- response: {"id":"1"}')
+  end
+
+  it "logs the request's headers at DEBUG level" do
+    connection(logger).post('/nigirizushi')
+    log.rewind
+
+    expect(log.readlines.join).to include('DEBUG -- response: Content-Type: "application/json"')
+  end
+
+  context "when set body log to true" do
+    it "logs the request's body at DEBUG level" do
+      connection(logger, log_options: { bodies: true }).post('/nigirizushi')
+      log.rewind
+
+      expect(log.readlines.join).to include('DEBUG -- response: {"id":"1"}')
+    end
+  end
+
+  context "when set headers log to false" do
+    it "doesn't log the request's headers" do
+      connection(logger, log_options: { headers: false }).post('/nigirizushi')
+      log.rewind
+
+      expect(log.readlines.join).not_to include('DEBUG -- response: Content-Type: "application/json"')
+    end
+  end
+
+  private
+
+  def connection(logger = nil, config={})
+    Faraday.new(url: "http://sushi.com") do |builder|
+      builder.request(:url_encoded)
+      builder.response(:log_filter, logger, config)
+      builder.adapter(:test) do |stub|
+        stub.get("/temaki") do
+          [200, { "Content-Type" => "text/plain" }, "temaki"]
+        end
+        stub.post("/nigirizushi") do
+          [200, { "Content-Type" => "application/json" }, "{\"id\":\"1\"}"]
+        end
+      end
+    end
+  end
+end

spec/spec_helper.rb
@@ -1,0 +1,8 @@
+lib = File.expand_path("../lib", __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require "faraday/log_filter"
+
+RSpec.configure do |config|
+  config.order = :random
+end